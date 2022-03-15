7dc493882511ff06584bc7df79e15c52c1c184ec
Artiukh Misha <artiukh.misha@gmail.com>
2022-03-15 15:56:46 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class Main {
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m" + "\u001B[40m";
+    public static final String RED = "\u001B[31m" + "\u001B[41m";
+    public static final String GREEN = "\u001B[32m" + "\u001B[42m";
+    public static final String YELLOW = "\u001B[33m" + "\u001B[43m";
+    public static final String BLUE = "\u001B[34m" + "\u001B[44m";
+    public static final String PURPLE = "\u001B[35m" + "\u001B[45m";
+    public static final String CYAN = "\u001B[36m" + "\u001B[46m";
+    public static final String WHITE = "\u001B[37m" + "\u001B[47m";
+
+
+    public static void main(String[] args) {
+        System.out.println(WHITE+"qqqqqqqqqqqqqqqqqqqqqqqqqqqqqq"+RESET);
+        System.out.println(WHITE+"q"+BLACK+"qq"+WHITE+"qqqqqqqqqqqqqqqqqqqqqqqq"+BLACK+"qq"+WHITE+"q"+RESET);
+        System.out.println(WHITE+"q"+BLACK+"qqqq"+WHITE+"qqqqqqqqqqqqqqqqqqqq"+BLACK+"qqqq"+WHITE+"q"+RESET);
+        System.out.println(WHITE+"q"+BLACK+"qqq"+YELLOW+"q"+BLACK+"q"+WHITE+"qqqqqqqqqqqqqqqqqq"+BLACK+"q"+YELLOW+"q"+BLACK+"qqq"+WHITE+"q"+RESET);
+        System.out.println(WHITE+"q"+BLACK+"qqq"+YELLOW+"qq"+BLACK+"qq"+WHITE+"qqqqqqqqqqqqqq"+BLACK+"qq"+YELLOW+"qq"+BLACK+"qqq"+WHITE+"q"+RESET);
+        System.out.println(WHITE+"qq"+BLACK+"qq"+YELLOW+"qqqq"+BLACK+"q"+WHITE+"qqqqqqqqqqqq"+BLACK+"q"+YELLOW+"qqqq"+BLACK+"qq"+WHITE+"qq"+RESET);
+        System.out.println(WHITE+"qq"+BLACK+"qq"+YELLOW+"qqqqq"+BLACK+"qq"+WHITE+"qq"+BLACK+"qqqq"+WHITE+"qq"+BLACK+"qq"+YELLOW+"qqqqq"+BLACK+"qq"+WHITE+"qq"+RESET);
+        System.out.println(WHITE+"qqq"+BLACK+"q"+YELLOW+"qqqqqqq"+BLACK+"qq"+YELLOW+"qqqq"+BLACK+"qq"+YELLOW+"qqqqqqq"+BLACK+"q"+WHITE+"qqq"+RESET);
+        System.out.println(WHITE+"qqq"+BLACK+"q"+YELLOW+"qqqqqqq"+BLACK+"qq"+YELLOW+"qqqq"+BLACK+"qq"+YELLOW+"qqqqqqq"+BLACK+"q"+WHITE+"qqq"+RESET);
+        System.out.println(WHITE+"qqqq"+BLACK+"q"+YELLOW+"qqqqqqqqqqqqqqqqqqqq"+BLACK+"q"+WHITE+"qqqq"+RESET);
+        System.out.println(WHITE+"qqqqq"+BLACK+"q"+YELLOW+"q"+BLACK+"q"+YELLOW+"qqqqqqqqqqqqqq"+BLACK+"q"+YELLOW+"q"+BLACK+"q"+WHITE+"qqqqq"+RESET);
+        System.out.println(WHITE+"qqqqqq"+BLACK+"q"+YELLOW+"qqqqqqqqqqqqqqqq"+BLACK+"q"+WHITE+"qqqqqq"+RESET);
+        System.out.println(WHITE+"qqqqqq"+BLACK+"q"+YELLOW+"qqqqqqqqqqqqqqqq"+BLACK+"q"+WHITE+"qqqqqq"+RESET);
+        System.out.println(WHITE+"qqqqq"+BLACK+"q"+YELLOW+"qqq"+BLACK+"qq"+YELLOW+"qqqqqqqq"+BLACK+"qq"+YELLOW+"qqq"+BLACK+"q"+WHITE+"qqqqq"+RESET);
+        System.out.println(WHITE+"qqqqq"+BLACK+"q"+YELLOW+"qq"+BLACK+"q"+WHITE+"q"+BLACK+"qq"+YELLOW+"qqqqqq"+BLACK+"qq"+WHITE+"q"+BLACK+"q"+YELLOW+"qq"+BLACK+"q"+WHITE+"qqqqq"+RESET);
+        System.out.println(WHITE+"qqqqq"+BLACK+"q"+YELLOW+"qq"+BLACK+"qqqq"+YELLOW+"qqqqqq"+BLACK+"qqqq"+YELLOW+"qq"+BLACK+"q"+WHITE+"qqqqq"+RESET);
+        System.out.println(WHITE+"qqqqq"+BLACK+"q"+YELLOW+"qqq"+BLACK+"qq"+YELLOW+"qqq"+BLACK+"qq"+YELLOW+"qqq"+BLACK+"qq"+YELLOW+"qqq"+BLACK+"q"+WHITE+"qqqqq"+RESET);
+        System.out.println(WHITE+"qqqq"+BLACK+"q"+YELLOW+"qq"+RED+"qq"+YELLOW+"qqqqqqqqqqqq"+RED+"qq"+YELLOW+"qq"+BLACK+"q"+WHITE+"qqqq"+RESET);
+        System.out.println(WHITE+"qqqq"+BLACK+"q"+YELLOW+"q"+RED+"qqqq" +YELLOW+"qq"+BLACK+"q"+YELLOW+"q"+BLACK+"qq"+YELLOW+"q"+BLACK+"q"+YELLOW+"qq"+RED+"qqqq"+YELLOW+"q"+BLACK+"q"+WHITE+"qqqq"+RESET);
+        System.out.println(WHITE+"qqqq"+BLACK+"q"+YELLOW+"q"+RED+"qqqq" +YELLOW+"qqq"+BLACK+"qqqq"+YELLOW+"qqq"+RED+"qqqq"+YELLOW+"q"+BLACK+"q"+WHITE+"qqqq"+RESET);
+        System.out.println(WHITE+"qqqqq"+BLACK+"q"+YELLOW+"q"+RED+"qq" +YELLOW+"qqqq"+BLACK+"q"+RED+"qq"+BLACK+"q"+YELLOW+"qqqq"+RED+"qq"+YELLOW+"q"+BLACK+"q"+WHITE+"qqqqq"+RESET);
+        System.out.println(WHITE+"qqqqqq"+BLACK+"q"+YELLOW+"qqqqqq"+BLACK+"q"+RED+"qq"+BLACK+"q"+YELLOW+"qqqqqq"+BLACK+"q"+WHITE+"qqqqqq"+RESET);
+        System.out.println(WHITE+"qqqqqqq"+BLACK+"qq"+YELLOW+"qqqqq"+BLACK+"qq"+YELLOW+"qqqqq"+BLACK+"qq"+WHITE+"qqqqqqq"+RESET);
+        System.out.println(WHITE+"qqqqqqqqq"+BLACK+"qqq"+YELLOW+"qqqqqq"+BLACK+"qqq"+WHITE+"qqqqqqqqq"+RESET);
+        System.out.println(WHITE+"qqqqqqqqqqqq"+BLACK+"qqqqqq"+WHITE+"qqqqqqqqqqqq"+RESET);
+        System.out.println(WHITE+"qqqqqqqqqqqqqqqqqqqqqqqqqqqqqq"+RESET);
+
+
+
+
+    }
+}
